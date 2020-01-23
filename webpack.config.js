4667ee832e112c8f39e368e2ae40602667232f3f
Jad <zeineddine.jad@gmail.com>
2020-01-23 11:49:00 -0500

    Converted landing page to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   watch: false, 
@@ -12,16 +13,41 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(html)$/,
+        loader: 'html-loader'
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          esModule: false
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()  
   ]
 };
 
